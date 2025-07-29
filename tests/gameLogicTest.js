94a169ddf8833da6d8594e1a1fc3bc740578d0ce
lunatek <128093480+tekluna@users.noreply.github.com>
2025-07-29 21:58:57 +0200

    fix: fixed bug in testfile and optimized gameengine a bit

--- tests/gameLogicTest.js
+++ tests/gameLogicTest.js
@@ -1,10 +1,12 @@
-import {updateScore} from "../service/tictactoeGameEngineService.js";
+const { updateScore } = require("../service/tictactoeGameEngineService.js");
 
 const player = "o";
 const board = ["x", "x", "o", "", "", "o", "", "", "x"];
 const selectedMove = 7;
 
+async function main(board, player, selectedMove){
+    const {newBoard, hasWon, isTie, nextPlayer} =  await updateScore(board, player, selectedMove);
+    console.log(newBoard, hasWon ,isTie, nextPlayer)
+}
 
-const {newBoard, hasWon, isTie, nextPlayer} =  await updateScore(board, player, selectedMove);
-console.log(newBoard, hasWon ,isTie, nextPlayer)
-
+main(board,player,selectedMove);
